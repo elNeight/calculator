0ec0dd34a2307bb537c1cf34d6d21011707856fd
Sergey Novitsky <novitsky.srg@gmail.com>
2023-08-25 10:27:17 +0300

    Refactored rounding issue

--- src/main/java/org/example/parser/ast/impl/binary/DivisionExpression.java
+++ src/main/java/org/example/parser/ast/impl/binary/DivisionExpression.java
@@ -1,9 +1,13 @@
 package org.example.parser.ast.impl.binary;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import org.example.parser.ast.Expression;
+import org.springframework.beans.factory.annotation.Value;
 
 public class DivisionExpression implements Expression {
+
+  private static final Integer scale = 10;
 
   private final Expression firstOperand;
   private final Expression secondOperand;
@@ -15,7 +19,7 @@ public class DivisionExpression implements Expression {
 
   @Override
   public BigDecimal evaluate() {
-    return firstOperand.evaluate().divide(secondOperand.evaluate());
+    return firstOperand.evaluate().divide(secondOperand.evaluate(), scale, RoundingMode.HALF_UP);
   }
 
 }
